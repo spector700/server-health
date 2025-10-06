216398f251ccdb7a3bc6d069e4d5b59445c99695
spector <72362399+spector700@users.noreply.github.com>
2025-10-06 18:32:44 -0500

    feat: add more functions for server:

--- src/main/kotlin/com/spector/server_health_monitor/controller/ServerController.kt
+++ src/main/kotlin/com/spector/server_health_monitor/controller/ServerController.kt
@@ -2,10 +2,14 @@ package com.spector.server_health_monitor.controller
 
 import com.spector.server_health_monitor.entity.Server
 import com.spector.server_health_monitor.service.ServerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/servers")
@@ -20,5 +24,20 @@ class ServerController(val serverService: ServerService) {
         @PostMapping
     fun create(@RequestBody request: CreateServerRequest): Server {
         return serverService.createServer(request.name, request.hostname, request.ipAddress, request.port, request.location)
+    }
+
+    @GetMapping
+    fun getAllServers(): List<Server> {
+        return serverService.getAllServers()
+    }
+
+    @GetMapping("/{id}")
+    fun getServerById(@PathVariable id: UUID): Server? {
+        return serverService.getServerById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteServerById(@PathVariable id: UUID): Server? {
+         return serverService.deleteServerById(id)
     }
 }
--- src/main/kotlin/com/spector/server_health_monitor/service/ServerService.kt
+++ src/main/kotlin/com/spector/server_health_monitor/service/ServerService.kt
@@ -2,7 +2,9 @@ package com.spector.server_health_monitor.service
 
 import com.spector.server_health_monitor.entity.Server
 import com.spector.server_health_monitor.repository.ServerRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class ServerService (private val repository: ServerRepository) {
@@ -16,5 +18,21 @@ class ServerService (private val repository: ServerRepository) {
             location = location
         )
         return repository.save(server)
+    }
+
+    fun getAllServers(): List<Server> {
+        return repository.findAll()
+    }
+
+    fun getServerById(id: UUID): Server? {
+        return repository.findByIdOrNull(id)
+    }
+
+    fun deleteServerById(id: UUID): Server? {
+        val result = getServerById(id)
+        if (result != null) {
+            repository.delete(result)
+        }
+        return null
     }
 }
