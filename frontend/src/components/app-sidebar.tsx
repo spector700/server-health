9557327c052ea3cb03b95193265c3de06caae9ba
spector <72362399+spector700@users.noreply.github.com>
2025-11-12 19:20:44 -0600

    fix(frontend): remove extra sidebar options

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,20 +1,16 @@
 import * as React from "react"
 import {
     IconDashboard,
-    IconHelp,
     IconInnerShadowTop,
     IconListDetails,
-    IconSearch,
     IconSettings,
 } from "@tabler/icons-react"
 
 import {NavMain} from "@/components/nav-main.tsx"
 import {NavSecondary} from "@/components/nav-secondary"
-import {NavUser} from "@/components/nav-user"
 import {
     Sidebar,
     SidebarContent,
-    SidebarFooter,
     SidebarHeader,
     SidebarMenu,
     SidebarMenuButton,
@@ -22,11 +18,6 @@ import {
 } from "@/components/ui/sidebar"
 
 const data = {
-    user: {
-        name: "shadcn",
-        email: "m@example.com",
-        avatar: "/avatars/shadcn.jpg",
-    },
     navMain: [
         {
             title: "Dashboard",
@@ -45,16 +36,6 @@ const data = {
             title: "Settings",
             url: "#",
             icon: IconSettings,
-        },
-        {
-            title: "Get Help",
-            url: "#",
-            icon: IconHelp,
-        },
-        {
-            title: "Search",
-            url: "#",
-            icon: IconSearch,
         },
     ],
 }
@@ -81,9 +62,6 @@ export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
                 <NavMain/>
                 <NavSecondary items={data.navSecondary} className="mt-auto"/>
             </SidebarContent>
-            <SidebarFooter>
-                <NavUser user={data.user}/>
-            </SidebarFooter>
         </Sidebar>
     )
 }
